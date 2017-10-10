c8b1f3905858b3b65aad814adff8ed92abf274c9
Marcus Rocha <sif.marcusvinicius@gmail.com>
2017-10-10 19:03:16 +0000

    Adicionando a action destroy e seus testes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -30,6 +30,13 @@ class Api::V1::UsersController < ApplicationController
 		end
 	end
 
+	def destroy
+		user = User.find(params[:id])
+		if user.destroy
+			head 204
+		end
+	end
+
 	private
 
 	def user_params
