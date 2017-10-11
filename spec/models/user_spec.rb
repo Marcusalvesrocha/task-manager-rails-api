77fa163d3c4be1f47efb288982db1275a142492d
Marcus Rocha <sif.marcusvinicius@gmail.com>
2017-10-11 03:17:26 +0000

    Adicionando token de autenticacao ao user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   
   let(:user) { build(:user)}
+  let(:token) { Devise.friendly_token }
 
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
@@ -12,9 +13,26 @@ RSpec.describe User, type: :model do
   describe '#info' do
     it 'returns email, created_at and token' do
     	user.save!
-    	allow(Devise).to receive(:friendly_token).and_return('adcf432dfgdhiTOKEN')
+    	allow(Devise).to receive(:friendly_token).and_return(token)
 
-    	expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: adcf432dfgdhiTOKEN")
+    	expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: #{token}")
     end
+  end
+
+  describe '#generate_authentication_token!' do
+  	it 'generates a unique auth token' do
+  		allow(Devise).to receive(:friendly_token).and_return(token)
+  		user.generate_authentication_token!
+
+  		expect(user.auth_token).to eq(token)
+  	end
+
+  	it 'generates another auth token when the current has been taken' do
+  		allow(Devise).to receive(:friendly_token).and_return(token, token, 'abcXYZ123')
+  		existing_user = create(:user)
+  		user.generate_authentication_token!
+
+  		expect(user.auth_token).not_to eq(existing_user.auth_token)
+  	end
   end
 end
